aa92e120c0b5a9eaad179f4d9cedc2ea38fed21e
PavlosKon <pavlos.kon@outlook.com>
2020-07-06 22:40:21 +0300

    Updates

---
+++ precache-manifest.fd0cb067ee13a1d96e1ba555da6a8e87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52af4a119765b33bf52ae122a1ac9df2",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "6618e5899f68a253bdd2",
+    "url": "/react-gh-pages/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "11235602ea0c1ce0a0da",
+    "url": "/react-gh-pages/static/js/2.04e7d5bf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-gh-pages/static/js/2.04e7d5bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6618e5899f68a253bdd2",
+    "url": "/react-gh-pages/static/js/main.5d035ac7.chunk.js"
+  },
+  {
+    "revision": "b3a067c21fefecd756fe",
+    "url": "/react-gh-pages/static/js/runtime-main.64a27a65.js"
+  }
+]);
